dd58f169bd2714ae77de65b68a68e22ec5d2b07f
Kevin Patel <patelkev@yahoo-inc.com>
2017-02-22 20:41:30 -0800

    Test HalfViewController

--- HalfTest/ViewController.swift
+++ HalfTest/ViewController.swift
@@ -19,7 +19,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination
+        destinationVC.modalPresentationStyle = UIModalPresentationStyle.custom
+        destinationVC.transitioningDelegate = self
+    }
 
 }
 
+extension ViewController : UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return HalfPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+}
+
+
+class SecondViewController: UIViewController {
+    
+    @IBAction func dissmissChild(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+class  HalfPresentationController: UIPresentationController {
+    override var frameOfPresentedViewInContainerView: CGRect {
+        if let containerViewBounds = containerView?.bounds {
+            return CGRect(x: 0, y: containerViewBounds.height/2, width: containerViewBounds.width, height: containerViewBounds.height/2)
+        }
+        return CGRect.zero
+    }
+}
